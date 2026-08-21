b9d6c3c3a1ff4b54edc114c8fc75b2e328a2fa06
Andres Prada <prada1295@gmail.com>
2026-08-21 17:45:46 -0500

    Initial Commit

---
+++ TransportTerminal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransportTerminal
+//
+//  Created by Andres Felipe Prada Chivata on 21/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TransportTerminalUITests/TransportTerminalUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  TransportTerminalUITestsLaunchTests.swift
+//  TransportTerminalUITests
+//
+//  Created by Andres Felipe Prada Chivata on 21/08/26.
+//
+
+import XCTest
+
+final class TransportTerminalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
